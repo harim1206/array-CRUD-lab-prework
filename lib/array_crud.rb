e0010828bf9bdf7f4f509a7284af53110f8bbb25
Harim Choi <harim1206@gmail.com>
2018-02-08 16:46:33 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  arr = ["Harry","Olivia","Junior","Lola"]
-  arr.unshift("Poo")
+
+  array.unshift(element)
 
 end
 
